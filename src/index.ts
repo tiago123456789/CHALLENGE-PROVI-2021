cd7960c6256aa71ace055de7b789bdf4536bd03f
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-07-06 23:31:09 -0300

    Change to get port setted per heroku

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import app from  "./config/Server"
 
-app.listen(process.env.PORT, () => console.log(`Server is running in address ${process.env.URL_APP}`))
+app.listen(process.env.PORT || 3000, () => console.log(`Server is running in address ${process.env.URL_APP}`))
